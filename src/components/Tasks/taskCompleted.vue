058bd4344ffbb2a8bcd4690e1f23ae2307b47577
Asaf Yeshoa <asafa097@gmail.com>
2021-02-06 13:36:00 +0200

    all fixed

--- src/components/Tasks/taskCompleted.vue
+++ src/components/Tasks/taskCompleted.vue
@@ -4,12 +4,10 @@
     enter-active-class="animated zoomIn"
     leave-active-class="animated zoomOut absolute-top"
   >
-  <div class="q-mt-lg" >
-  <q-list separator v-if="Object.keys(tasksCompleted).length" >
+  <q-list separator v-if="Object.keys(tasksCompleted).length" class="q-mt-lg" >
     <banner  bgColor="bg-light-green-5" >Completed Tasks</banner>
     <task  v-for="(task, key) in tasksCompleted" :key=key :task="task" :id="key"/>
   </q-list>
-  </div>
   </transition>
 </template>
 
